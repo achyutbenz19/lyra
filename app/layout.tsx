fb409085177db1b86b6cf9018dcf61c92c14ab9b
achyutbenz19 <achyut.benz@gmail.com>
2024-03-27 14:17:02 -0400

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { AI } from "./api/actions";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "lyra",
@@ -16,17 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={GeistSans.className}>
-        <AI>
-          <ThemeProvider attribute="class">
-            <div className="flex flex-col min-h-screen">
-              <main className="flex flex-col flex-1 bg-muted/50 dark:bg-background">
-                {children}
-              </main>
-            </div>
-          </ThemeProvider>
-        </AI>
-      </body>
+      <ClerkProvider>
+        <body className={GeistSans.className}>
+          <AI>
+            <ThemeProvider attribute="class">
+              <div className="flex flex-col min-h-screen">
+                <main className="flex flex-col flex-1 bg-muted/50 dark:bg-background">
+                  {children}
+                </main>
+              </div>
+            </ThemeProvider>
+          </AI>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
