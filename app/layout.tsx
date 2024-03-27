e2ceb69fb060b674c19160209a263a9da93c1b4c
achyutbenz19 <achyut.benz@gmail.com>
2024-03-27 14:53:51 -0400

    import { Analytics }

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { AI } from "./api/actions";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "lyra",
@@ -24,6 +25,7 @@ export default function RootLayout({
               <div className="flex flex-col min-h-screen">
                 <main className="flex flex-col flex-1 bg-muted/50 dark:bg-background">
                   {children}
+                  <Analytics />
                 </main>
               </div>
             </ThemeProvider>
