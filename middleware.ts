3708e6b7f787fdb102d0f405e1f92186f0519d0b
achyutbenz19 <achyut.benz@gmail.com>
2024-04-02 12:56:08 -0400

    middleware new matcher

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
